5a2ecea076e0b802b61a2a05dabd70cbb9585d18
Lucas <lucaarruda4@gmail.com>
2022-06-27 18:34:33 -0300

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
+
+function App() {
+  return (
+    <>
+      <h1>Contador</h1>
+      <Ifoodcounter/>
+    </>
+  );
+}
+
+export default App;
